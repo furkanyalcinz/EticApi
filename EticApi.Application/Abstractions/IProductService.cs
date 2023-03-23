23a4fc2706f2db479d2946cc142034b871a57631
Furkan Yalçın <76597372+furkanyalcinz@users.noreply.github.com>
2023-03-24 01:20:13 +0300

    Add project files.

---
+++ EticApi.Application/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using EticApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EticApi.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
